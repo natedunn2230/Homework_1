005e93fb664bd08f4762ab8659c38455efa01341
natedunn2230 <dunn.113@wright.edu>
2018-09-03 16:41:48 -0400

    Got doodle board working fairly well.

--- app/src/main/java/com/nathandunn/homework_1/DoodleBoard.java
+++ app/src/main/java/com/nathandunn/homework_1/DoodleBoard.java
@@ -13,13 +13,15 @@ public class DoodleBoard extends View {
 
     private Path path = new Path();
     private Paint tool = new Paint();
-
+    final float ALPHA = 1.0f;
+    private float xpos = 0, ypos = 0;
     public DoodleBoard(Context con, AttributeSet attSet){
         super(con, attSet);
 
+        //set attributes of the tool that draws to the canvas
         tool.setARGB(255,255,0,0);
         tool.setStyle(Paint.Style.STROKE);
-        tool.setStrokeWidth(7.0f);
+        tool.setStrokeWidth(15.0f);
         tool.setAntiAlias(true);
         tool.setStrokeJoin(Paint.Join.ROUND);
     }
@@ -31,30 +33,51 @@ public class DoodleBoard extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float currentX = event.getX();
-        float currentY = event.getY();
+        float eventX = event.getX();
+        float eventY = event.getY();
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE:
-                path.moveTo(currentX, currentY);
+               action_move(eventX, eventY);
                 Log.d("touch_event:", "action_move");
                 break;
             case MotionEvent.ACTION_DOWN:
-                path.reset();
-                path.lineTo(currentX, currentY);
+                action_down(eventX, eventY);
                 Log.d("touch_event", "action_down");
-                postInvalidate();
                 break;
             case MotionEvent.ACTION_UP:
-                path.lineTo(currentX, currentY);
-                path.reset();
+                action_up();
                 Log.d("touch_event", "action_up");
                 break;
         }
 
-        Log.d("touch_coords", String.format("(%f, %f)", currentX, currentY));
+        Log.d("touch_coords", String.format("(%f, %f)", xpos, ypos));
         postInvalidate();
         return true;
+    }
+
+    private void action_move(float x, float y){
+        float diffX = Math.abs(x - xpos);
+        float diffY = Math.abs(y - ypos);
+
+        if (diffX >= ALPHA|| diffY >= ALPHA) {
+            path.quadTo(xpos, ypos,(x + xpos)/2, (y + ypos)/2);
+            xpos = x;
+            ypos = y;
+        }
+        invalidate();
+    }
+
+    private void action_down(float x, float y){
+        xpos = x;
+        ypos = y;
+        path.moveTo(x, y);
+        invalidate();
+    }
+
+    private void action_up(){
+        path.moveTo(xpos, ypos);
+        invalidate();
     }
 
 
